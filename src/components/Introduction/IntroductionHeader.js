e0905495549970927c8c523bdd437513db4614b5
yeonsuBaek <soothbugger@gmail.com>
2023-01-08 18:36:56 +0900

    style: 상하 margin 추가

--- src/components/Introduction/IntroductionHeader.js
+++ src/components/Introduction/IntroductionHeader.js
@@ -1,6 +1,6 @@
 const IntroductionHeader = () => {
   return (
-    <header className="px-sm-col w-sm-col-2">
+    <header className="px-sm-col w-sm-col-2 mt-8 mb-4">
       <h1 className="text-primary text-header-1 font-bold leading-tight">
         안녕하세요
         <br />
