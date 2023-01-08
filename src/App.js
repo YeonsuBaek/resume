8c3b66ab70a60ac5b86740a42e41a8c3171ec9a8
yeonsuBaek <soothbugger@gmail.com>
2023-01-08 18:37:29 +0900

    feat: 자기소개 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import IntroductionHeader from "./components/Introduction/IntroductionHeader";
+import IntroductionDescription from "./components/Introduction/IntroductionDescription";
 
 function App() {
   return (
     <div className="w-full px-sm-container mx-auto">
       <div className="flex flex-wrap">
         <IntroductionHeader />
+        <IntroductionDescription />
       </div>
     </div>
   );
---
+++ src/components/Introduction/IntroductionDescription.js
@@ -0,0 +1,14 @@
+const IntroductionDescription = () => {
+  return (
+    <div className="px-sm-col w-sm-col-2 mb-16">
+      <p className="text-primary text-body-lg leading-normal">
+        저는 이런사람이고 저런사람이고 이렇고 저렇고 이런사람이고 저런사람이고 이렇고 저렇고 이런사람이고 저런사람이고
+        이렇고 저렇고 이런사람이고 저런사람이고 이렇고 저렇고 이런사람이고 저런사람이고 이렇고 저렇고 이런사람이고
+        저런사람이고 이렇고 저렇고 이런사람이고 저런사람이고 이렇고 저렇고 이런사람이고 저런사람이고 이렇고 저렇고
+        이런사람입니다
+      </p>
+    </div>
+  );
+};
+
+export default IntroductionDescription;
