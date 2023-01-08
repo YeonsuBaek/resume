135e487aa3dc1abc765a370475a958a65be78302
yeonsuBaek <soothbugger@gmail.com>
2023-01-08 20:40:25 +0900

    feat: Project 데이터를 ProjectItem에 전달하는 기능 구현

--- src/components/Project/ProjectList.js
+++ src/components/Project/ProjectList.js
@@ -1,11 +1,22 @@
 import ProjectItem from "./ProjectItem";
 
+import project from "../../data/project";
+
 const ProjectList = () => {
   return (
     <div>
-      <ProjectItem />
-      <ProjectItem />
-      <ProjectItem />
+      {project.map((item) => {
+        return (
+          <ProjectItem
+            title={item.title}
+            description={item.description}
+            date={item.date}
+            details={item.details}
+            techStacks={item.techStacks}
+            links={item.links}
+          />
+        );
+      })}
     </div>
   );
 };
---
+++ src/data/project.js
@@ -0,0 +1,28 @@
+const project = [
+  {
+    title: "식메추",
+    description:
+      "대락 이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러 이런 일을 했습니다.",
+    date: "2022.12",
+    details: ["디자인", "어쩌구", "저쩌구"],
+    techStacks: ["JavaScript", "HTML", "Tailwind CSS"],
+    links: {
+      demo: "/",
+      github: "/",
+    },
+  },
+  {
+    title: "지금 한신은",
+    description:
+      "대락 이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러이러 이런 일을 했습니다.",
+    date: "2022.07-2022.11",
+    details: ["디자인", "어쩌구", "저쩌구"],
+    techStacks: ["Vue.js", "Sass"],
+    links: {
+      demo: "/",
+      github: "/",
+    },
+  },
+];
+
+export default project;
