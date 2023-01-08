3b1466e086e5b01ddbf3c5cacc9790618afcb73e
yeonsuBaek <soothbugger@gmail.com>
2023-01-08 18:19:16 +0900

    style: 모바일 버전 그리드 사이즈 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-full px-sm-container mx-auto">
+      <div className="flex flex-wrap">
+        <div className="px-sm-col w-sm-col-2">h1</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        "sm-container": "20px",
+        "sm-col": "4px",
+      },
+      spacing: {
+        "sm-col-1": "50%",
+        "sm-col-2": "100%",
+      },
+    },
   },
   plugins: [],
 };
