e8e17ff3bded6766eb63a2a5493d181ad3e5725a
yeonsuBaek <soothbugger@gmail.com>
2023-01-08 18:29:03 +0900

    feat: 이력서 Header 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import IntroductionHeader from "./components/Introduction/IntroductionHeader";
+
 function App() {
   return (
     <div className="w-full px-sm-container mx-auto">
       <div className="flex flex-wrap">
-        <div className="px-sm-col w-sm-col-2">h1</div>
+        <IntroductionHeader />
       </div>
     </div>
   );
---
+++ src/components/Introduction/IntroductionHeader.js
@@ -0,0 +1,13 @@
+const IntroductionHeader = () => {
+  return (
+    <header className="px-sm-col w-sm-col-2">
+      <h1 className="text-primary text-header-1 font-bold leading-tight">
+        안녕하세요
+        <br />
+        백연수입니다.
+      </h1>
+    </header>
+  );
+};
+
+export default IntroductionHeader;
